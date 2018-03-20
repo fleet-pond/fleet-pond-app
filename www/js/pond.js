c83edc4a821955c7e408bdf528981dd5d86fbb7d
Matt Weeks <m_weeks@hotmail.co.uk>
2018-03-20 21:00:07 +0000

    Add bench markers. Add poi marker links.

--- www/js/pond.js
+++ www/js/pond.js
@@ -1,5 +1,10 @@
 var poi;
 var showDelay = 180;
+var poiMarkers = [];
+var benchMarkers = [];
+var activeInfoWindow = null;
+var infoWindows = [];
+
 $(function() {
     $.getJSON( "points-of-interest.json", {
         format: "json"
@@ -9,7 +14,7 @@ $(function() {
             var getKey = json[key];
             getKey.forEach(function(item) {
                 var routesHTML = getRoutesHTML(item);
-                $("#points-of-interest-info").before('<a data-toggle="tab" href=#selected-points-of-interest onclick="selectedPoI(\'' +
+                $("#points-of-interest-info").before('<a data-toggle="tab" href="#selected-points-of-interest" onclick="selectedPoI(\'' +
                     item.number + '\', \'#points-of-interest\');"><div class="pointOfInterest card"><div class="poiPic"><img src="images/' + item.image +
                     '.thumbnail"/></div>' +
                     '<div class="cardText poiText"><b>' + item.name +
@@ -17,18 +22,26 @@ $(function() {
                     '</div><div class="clear"></div></div></a>');
 
                 var position = {lat: item.latitude, lng: item.longitude};
-                var marker = new google.maps.Marker({
+                poiMarkers.push(new google.maps.Marker({
                     position: position,
+                    // icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
 		            label: item.number,
                     map: map,
                     animation: google.maps.Animation.DROP
-                });
-                var infoWindow = new google.maps.InfoWindow({
-                    content: item.name
-                    //content: '<a href="#selected-points-of-interest" onclick="selectedPoI(' + item.number + ')">' + item.name + '</a>'
-                });
-                marker.addListener('click', function() {
-                    infoWindow.open(map, marker);
+                }));
+                infoWindows.push(new google.maps.InfoWindow({
+                    content: '<a data-toggle="tab" href=#selected-points-of-interest onclick="selectedPoI(\'' + item.number + '\', \'#mapFrame\');">' + item.name + '</a>'
+                }));
+                var pointer = poiMarkers.length - 1;
+                poiMarkers[poiMarkers.length-1].addListener('click', function() {
+                    if (activeInfoWindow != null) {
+                        infoWindows[activeInfoWindow].close();
+                    }
+                    console.log(activeInfoWindow);
+                    infoWindows[pointer].open(map, poiMarkers[pointer]);
+                    infoWindows[pointer].setMap(map);
+                    activeInfoWindow = pointer;
+                    console.log(activeInfoWindow);
                 })
             });
         }
@@ -50,6 +63,16 @@ $(function() {
         }
     });
 
+    benchCoordinates.forEach(function(element) {
+        var position = {lat: element.lat, lng: element.lng};
+        benchMarkers.push(new google.maps.Marker({
+            position: position,
+            icon: "http://maps.google.com/mapfiles/ms/icons/blue.png",
+            size: new google.maps.Size(60, 100),
+            animation: google.maps.Animation.DROP
+        }));
+    });
+
 	$('#zoom-in').click(function () {
         $('#pond-map').width($('#pond-map').width()*1.2)
         $('#pond-map').height($('#pond-map').height()*1.2)
@@ -66,25 +89,26 @@ function selectedPoI(number, link) {
             if (index == 0) {
                 $("#poi-previous").hide();
                 $("#poi-next").show();
-                $("#poi-next").attr("onclick", "selectedPoI('" + poi.points_of_interest[index + 1].number + "');");
+                $("#poi-next").attr("onclick", "selectedPoI('" + poi.points_of_interest[index + 1].number + "', '" + link + "');");
             }
             else if (index == poi.points_of_interest.length - 1) {
                 $("#poi-previous").show();
                 $("#poi-next").hide();
-                $("#poi-previous").attr("onclick", "selectedPoI('" + poi.points_of_interest[index - 1].number + "');");
+                $("#poi-previous").attr("onclick", "selectedPoI('" + poi.points_of_interest[index - 1].number + "', '" + link + "');");
             }
             else {
                 $("#poi-previous").show();
                 $("#poi-next").show();
-                $("#poi-previous").attr("onclick", "selectedPoI('" + poi.points_of_interest[index - 1].number + "');");
-                $("#poi-next").attr("onclick", "selectedPoI('" + poi.points_of_interest[index + 1].number + "');");
+                $("#poi-previous").attr("onclick", "selectedPoI('" + poi.points_of_interest[index - 1].number + "', '" + link + "');");
+                $("#poi-next").attr("onclick", "selectedPoI('" + poi.points_of_interest[index + 1].number + "', '" + link + "');");
             }
 
             $("#poi-heading").html('<a id="poi-back" data-toggle="tab" href="#points-of-interest" onclick="scrollToTop();showNavBar(true);"><i class="fas fa-angle-double-left"></i></a> ' + element.name);
             $("#poi-back").attr("href", link);
             $("#poi-name").html("Point of interest " + number);
             $("#poi-image").attr("src", 'images/' + element.image);
-            $("#poi-description").html("<b>Route access: </b>" + getRoutesHTML(element) + "<br>" + element.description);
+            var viewOnMapHTML = '<a data-toggle="tab" href="#mapFrame" onclick="clickMarker(poiMarkers[' + index + ']);showNavBar(true);$(\'#mapMenuItem\')" ><i class="fas fa-map"></i> View on map</a><br>';
+            $("#poi-description").html(viewOnMapHTML + "<b>Route access: </b>" + getRoutesHTML(element) + "<br>" + element.description);
             showNavBar(false);
         }
     });
@@ -109,7 +133,7 @@ var yellowPath;
 var greenPath;
 var brownPath;
 var overlayOn=true;
-var gpsOptions = { timeout: 10000 };
+var gpsOptions = { timeout: 20000 };
 var consecutiveLocationFails = 0;
 
 function initMap() {
@@ -180,6 +204,7 @@ function initMap() {
         });
     });
 
+    $("#checkBoxInterests").click();
     $("#checkBoxBlue").click();
     $("#checkBoxYellow").click();
     $("#checkBoxRed").click();
@@ -226,12 +251,12 @@ function updateGPSLocation(userPosition) {
         navigator.geolocation.getCurrentPosition(function(pos) {
             console.log(pos.coords.latitude + "\n" + pos.coords.longitude);
             userPosition.setPosition(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude))
-            setTimeout(function() { updateGPSLocation(userPosition); }, 5000);
+            setTimeout(function() { updateGPSLocation(userPosition); }, 3000);
         },
         function() {
             console.log("NOPE - timeout")
             consecutiveLocationFails += 1;
-            if (consecutiveLocationFails < 10) {
+            if (consecutiveLocationFails < 100) {
                 setTimeout(function() { updateGPSLocation(userPosition); }, 5000);
             }
             else {
@@ -272,5 +297,18 @@ function showRoute(route) {
 function checkShowRoute(route, shownRoute) {
     if ($('#checkBox' + route).prop('checked') != (route == shownRoute)) {
         $("#checkBox" + route).click();
+    }
+}
+
+function toggleMarkers(markers, show) {
+    for (i = 0; i < markers.length; i++) {
+        console.log(markers[i])
+        toggleMapItem(markers[i], show);
+    }
+}
+
+function clickMarker(marker) {
+    if (infoWindows[activeInfoWindow].getMap() != undefined) {
+        google.maps.event.trigger(marker, 'click');
     }
 }
